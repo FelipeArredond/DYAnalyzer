77811cc6e70b9428734b51b597d8dc331d8ce9f1
FelipeArredond <pipechiqui77@gmail.com>
2023-08-13 01:15:28 -0500

    Added trivy to gh actions workflow

--- analyzer/urls.py
+++ analyzer/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.hello)
+    path('', views.hello),
+    path('/hello2', views.hello2),
+    path('/trivy', views.trivy)
 ]
--- dockanalyzer/urls.py
+++ dockanalyzer/urls.py
@@ -16,9 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from analyzer.views import hello
+from analyzer.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('analyzer.urls'))
+    path('', hello)
 ]
